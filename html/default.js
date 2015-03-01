aae9e06dd3eebabf42f468eeb104d3aa4df32c55
pakimchy <pakimchy1@gmail.com>
2015-03-01 14:30:35 +0900

    150301
    자바 변수 선언

--- html/default.js
+++ html/default.js
@@ -1,14 +1,17 @@
 jQuery(function($){
-	$(".gnb>button").on("click", function(){
+	var	$gnbBtn = $(".gnb>button");
+	// 반복되는 이름을 만들어준다.
+	// 변수를 선언하지 않으면 지속적으로 찾아봐야하지만, 변수를 선언하면 한번만 찾는다.
+	$gnbBtn.on("click", function(){
 		// $(".gnb>ul").slideToggle(200);
 		// slideToggle은 기본값을 400ms을 가진다.
 		$(this).next("ul").slideToggle(200);
 		// this는 섵개한 개체, next는 바로 다음에 올 것을 말하며, 이미 알고 있을 경우 ul을 생략해도 무관하다.
-		if( $(".gnb>button").text() == "메뉴 열기" ) {
-			$(".gnb>button").text("메뉴 닫기");
+		if( $gnbBtn.text() == "메뉴 열기" ) {
+			$gnbBtn.text("메뉴 닫기");
 			alert("닫기");
 		}else{
-			$(".gnb>button").text("메뉴 열기");
+			$gnbBtn.text("메뉴 열기");
 			alert("열기");
 		}
 	});	
